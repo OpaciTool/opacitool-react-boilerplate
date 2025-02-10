fbdae1cc59dcdbec612647f2b7096a84e087ffec
Aneta Dimoska <aneta.d@brainster.co>
2025-02-10 12:27:35 +0100

    Added content and style for Lecture 3 of Module 7

--- src/features/training/components/sections/EmissionFrequency.tsx
+++ src/features/training/components/sections/EmissionFrequency.tsx
@@ -6,17 +6,12 @@ export default function EmissionFrequency() {
     const { fields } = formContent.method22Section;
     return (
       <div className="px-4 lg:px-14">
-        <div className="grid grid-cols-2 border border-gray-300">
+        <div className="grid grid-cols-1 lg:grid-cols-2 ">
           {/* Left Column - Logo Section */}
-          <div className="flex flex-col items-center justify-center p-6">
+          <div className="flex flex-col order-2 lg:order-1 items-center justify-center p-6">
             {/* Logo Container */}
-            <div className="relative w-48">
-              <div className="overflow-hidden rounded-lg bg-[#40E0D0] p-4">
-                <div className="space-y-2 text-center">
-                  <h2 className="text-2xl font-bold text-white">OPAC TOOL</h2>
-                  <p className="text-sm text-white">OPACITY ASSESSMENT SYSTEM</p>
-                </div>
-              </div>
+            <div className="relative ">
+              <img src="/images/Logo.png" alt="OPAC TOOL" />
             </div>
             {/* Website URL */}
             <a
@@ -30,29 +25,29 @@ export default function EmissionFrequency() {
           </div>
   
           {/* Right Column - Form Fields */}
-          <div className="border-l border-gray-300">
+          <div className="border-x border-b border-black order-1 lg:order-2">
             {/* Emission Frequency */}
-            <div className="grid grid-cols-2 border-b border-gray-300">
+            <div className="grid grid-cols-2 border-b border-black cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.emission_frequency)}>
               <div className="bg-gray-900 p-3">
                 <h3 className="text-sm font-medium text-white">Emission Frequency</h3>
                 <p className="mt-1 text-xs text-gray-300">(Total Emission Duration/Total Observation Duration) x 100%</p>
               </div>
-              <div className="border-l border-gray-300"></div>
+              <div className="border-l border-black"></div>
             </div>
   
             {/* Observer Name */}
-            <div className="border-b border-gray-300 p-3">
-              <div className="text-sm italic text-gray-600">Observer Name</div>
+            <div className="border-b border-black p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observer_name_company)}>
+              <div className="text-lg  text-zinc-900">Observer Name</div>
             </div>
   
             {/* Observer Signature */}
-            <div className="border-b border-gray-300 p-3">
-              <div className="text-sm italic text-gray-600">Observer Signature</div>
+            <div className="border-b border-black p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observer_name_company)}>
+              <div className="text-lg  text-zinc-900">Observer Signature</div>
             </div>
   
             {/* Company/Organization */}
-            <div className="p-3">
-              <div className="text-sm italic text-gray-600">Company/Organization</div>
+            <div className="p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observer_name_company)}>
+              <div className="text-lg  text-zinc-900">Company/Organization</div>
             </div>
           </div>
         </div>
--- src/features/training/components/sections/ObservationInfoEPA.tsx
+++ src/features/training/components/sections/ObservationInfoEPA.tsx
@@ -12,7 +12,7 @@ export default function ObservationInfoEPA() {
           <div className="col-span-5 bg-gray-900 p-3">
             <h2 className="text-lg font-bold text-white">OBSERVATION INFO</h2>
           </div>
-          <div className="border-l border-black p-3">
+          <div className="border-l border-black p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observation_test_date)}>
             <div className="text-sm italic text-gray-600">Test Date</div>
           </div>
         </div>
@@ -22,16 +22,16 @@ export default function ObservationInfoEPA() {
           <div className="col-span-3 p-3">
             <div className="text-sm font-medium">Location</div>
           </div>
-          <div className="col-span-3 p-3">
+          <div className="col-span-3 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}>
             <div className="text-sm font-medium">Clock Time</div>
           </div>
-          <div className="col-span-3 p-3">
+          <div className="col-span-3 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observation_duration_column)}>
             <div className="text-center text-sm font-medium">
               Observation Duration
               <div className="text-xs">(min:sec)</div>
             </div>
           </div>
-          <div className="col-span-3 p-3">
+          <div className="col-span-3 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.emission_duration_column)}>
             <div className="text-center text-sm font-medium">
               Emission Duration
               <div className="text-xs">(min:sec)</div>
@@ -45,7 +45,7 @@ export default function ObservationInfoEPA() {
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3">
             <div className="text-sm">Location 1 - Start</div>
           </div>
-          <div className="col-span-3 border-r border-black bg-gray-50 p-3 border-t "></div>
+          <div className="col-span-3 border-r border-black bg-gray-50 p-3 border-t cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}></div>
           <div className="col-span-3 b bg-[#E4EAED] p-3"></div>
           <div className="col-span-3 bg-[#E4EAED] p-3"></div>
         </div>
@@ -54,16 +54,16 @@ export default function ObservationInfoEPA() {
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3">
             <div className="text-sm">Location 1 - Stop</div>
           </div>
-          <div className="col-span-3 border-r border-t  border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 border-t border-b border-black bg-gray-50 p-3"></div>
+          <div className="col-span-3 border-r border-t  border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}></div>
+          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observation_duration_column)}></div>
+          <div className="col-span-3 border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.emission_duration_column)}></div>
         </div>
 
         <div  className="grid grid-cols-12 ">
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3">
             <div className="text-sm">Location 2 - Start</div>
           </div>
-          <div className="col-span-3 border-r border-black bg-gray-50 p-3 border-t "></div>
+          <div className="col-span-3 border-r border-black bg-gray-50 p-3 border-t cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}></div>
           <div className="col-span-3  bg-[#E4EAED] p-3"></div>
           <div className="col-span-3 bg-[#E4EAED] p-3"></div>
         </div>
@@ -72,16 +72,16 @@ export default function ObservationInfoEPA() {
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3">
             <div className="text-sm">Location 2 - Stop</div>
           </div>
-          <div className="col-span-3 border-r border-t  border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 border-t border-b border-black bg-gray-50 p-3"></div>
+          <div className="col-span-3 border-r border-t  border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}></div>
+          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observation_duration_column)}></div>
+          <div className="col-span-3 border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.emission_duration_column)}></div>
         </div>
 
         <div  className="grid grid-cols-12 ">
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3">
             <div className="text-sm">Location 3 - Start</div>
           </div>
-          <div className="col-span-3 border-r border-black bg-gray-50 p-3 border-t "></div>
+          <div className="col-span-3 border-r border-black bg-gray-50 p-3 border-t cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}></div>
           <div className="col-span-3 b bg-[#E4EAED] p-3"></div>
           <div className="col-span-3 bg-[#E4EAED] p-3"></div>
         </div>
@@ -90,9 +90,9 @@ export default function ObservationInfoEPA() {
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3">
             <div className="text-sm">Location 3 - Stop</div>
           </div>
-          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 border-t border-b border-black bg-gray-50 p-3"></div>
+          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.clock_time)}></div>
+          <div className="col-span-3 border-r border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observation_duration_column)}></div>
+          <div className="col-span-3 border-t border-b border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.emission_duration_column)}></div>
         </div>
 
         {/* Total Row */}
@@ -101,8 +101,8 @@ export default function ObservationInfoEPA() {
             <div className="text-sm font-medium"></div>
           </div>
           <div className="col-span-3 border-r border-black bg-[#E4EAED] p-3 text-right">Total</div>
-          <div className="col-span-3 border-r border-black bg-gray-50 p-3"></div>
-          <div className="col-span-3 bg-gray-50 p-3"></div>
+          <div className="col-span-3 border-r border-black bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.observation_duration_total)}></div>
+          <div className="col-span-3 bg-gray-50 p-3 cursor-pointer hover:bg-gray-100" onClick={() => openModal(fields.emission_duration_total)}></div>
         </div>
       </div>
     </div>
